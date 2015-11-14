12010e72a00f7cfc84139d08ad05b93674751d26
Eric Lefort <leforte@mcmaster.ca>
2015-11-14 15:10:19 -0500

    LandingGearState enum added

---
+++ src/physicalSystems/Airplane.java
@@ -0,0 +1,14 @@
+package physicalSystems;
+/**
+ * @author Eric Le Fort
+ * @version 1.0
+ */
+
+public class Airplane{
+	private Engine[] engines;
+	private Wings wings;
+	private GPS gps;
+	private Gyrocompass gyrocompass;
+	private boolean seatbeltsOn;
+	
+}//Airplane
---
+++ src/physicalSystems/LandingGearState.java
@@ -0,0 +1,6 @@
+package physicalSystems;
+/**
+ * @author Eric Le Fort, Christopher McDonald
+ *
+ */
+public enum LandingGearState{ DEPLOYED, DEPLOYING, NOT_DEPLOYED }//LandingGearState
